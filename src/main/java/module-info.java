a26c523a183b64dd338ce8a8a476cdddcd2e36d0
youneschaguer <chagueryounes@gmail.com>
2023-11-14 17:01:55 +0100

    first commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,6 @@ module ensisa.puissance4 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-
     opens ensisa.puissance4 to javafx.fxml;
     exports ensisa.puissance4;
 }
