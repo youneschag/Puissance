f63167a061fcff5684f12cf6fa1bb6f2f0664450
Chaguer <chagueryounes@gmail.com>
2023-11-14 17:54:24 +0100

    Final commit on 17:54

--- src/main/java/ensisa/puissance4/MainController.java
+++ src/main/java/ensisa/puissance4/MainController.java
@@ -3,7 +3,7 @@ package ensisa.puissance4;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module ensisa.puissance4 {
     requires javafx.controls;
     requires javafx.fxml;
-//yhugjezgfeyugfzeg
-    //gdjhsdgfjhsdfzjhfjed
     opens ensisa.puissance4 to javafx.fxml;
     exports ensisa.puissance4;
 }
