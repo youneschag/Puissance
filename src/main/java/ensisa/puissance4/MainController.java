37ad1d3edd877f8ad35dca6aa6f773ca010829a1
Chaguer <chagueryounes@gmail.com>
2023-11-14 22:41:25 +0100

    Début interface avec fonctionnement bouton exit, et non fonctionnement bouton new game.

--- src/main/java/ensisa/puissance4/MainController.java
+++ src/main/java/ensisa/puissance4/MainController.java
@@ -1,14 +1,42 @@
 package ensisa.puissance4;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    private void handleExitButtonAction(ActionEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
+    @FXML
+    private Circle jetonJoueur1;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Circle jetonJoueur2;
+
+    // Méthode pour placer le jeton du joueur 1
+    public void placerJetonJoueur1() {
+        // Logique pour placer le jeton du joueur 1
+    }
+
+    // Méthode pour placer le jeton du joueur 2
+    public void placerJetonJoueur2() {
+        // Logique pour placer le jeton du joueur 2
+    }
+    @FXML
+    private Label labelJoueur1;
+
+    @FXML
+    private Label labelJoueur2;
+
+    // Méthode pour initialiser les éléments de jeu
+    public void initialiserJeu() {
+        labelJoueur1.setText("Joueur 1 :");
+        labelJoueur2.setText("Joueur 2 :");
+        // Autres initialisations...
     }
 }
--- src/main/java/ensisa/puissance4/PuissanceApplication.java
+++ src/main/java/ensisa/puissance4/PuissanceApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class PuissanceApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PuissanceApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(PuissanceApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 600);
+        stage.setTitle("Puissance 4");
         stage.setScene(scene);
         stage.show();
     }
