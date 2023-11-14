c8ba8bb4be31d933efbe7227f56d76f03b51f3dd
maria <aitbouserhanem@gmail.com>
2023-11-14 17:37:59 +0100

    message essai

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module ensisa.puissance4 {
     requires javafx.controls;
     requires javafx.fxml;
-
+//yhugjezgfeyugfzeg
     opens ensisa.puissance4 to javafx.fxml;
     exports ensisa.puissance4;
 }
