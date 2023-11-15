000b3fcb15ea82a56d6b4a6cbd5576653e21ba99
Chaguer <chagueryounes@gmail.com>
2023-11-15 09:15:39 +0100

    Ajout de mouse click sur la grille, et ajout de jeton pas encore fonctionnel avec alternance entre les 2 couelurs dans la grille

--- src/main/java/ensisa/puissance4/MainController.java
+++ src/main/java/ensisa/puissance4/MainController.java
@@ -3,7 +3,11 @@ package ensisa.puissance4;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -35,6 +39,8 @@ public class MainController {
 
     @FXML
     private Label labelJoueur2;
+    @FXML
+    private GridPane gridPane;
 
     // Méthode pour initialiser les éléments de jeu
     @FXML
@@ -56,4 +62,44 @@ public class MainController {
         int seconds = secondsElapsed % 60;
         durationLabel.setText(String.format("Durée: %02d:%02d", minutes, seconds));
     }
+    @FXML
+    private void handleColumnClick(MouseEvent event) {
+        Circle clickedCircle = (Circle) event.getSource();
+        int columnIndex = GridPane.getColumnIndex(clickedCircle);
+
+        // Logique pour placer le jeton dans la colonne columnIndex
+        placerJeton(columnIndex);
+    }
+    public void placerJeton(int columnIndex) {
+        int row = trouverLigneVide(columnIndex);
+
+        // Vérifier si la colonne est pleine
+        if (row != -1) {
+            // Placer le jeton dans la grille
+            Circle jeton = new Circle(25, Color.YELLOW); // Remplacez Color.YELLOW par la couleur du joueur actif
+            GridPane.setColumnIndex(jeton, columnIndex);
+            GridPane.setRowIndex(jeton, row);
+            gridPane.getChildren().add(jeton);
+        }
+    }
+
+    private int trouverLigneVide(int columnIndex) {
+        for (int row = 6; row >= 0; row--) { // Commencez depuis la dernière ligne
+            Circle jeton = getJeton(columnIndex, row);
+            if (jeton == null) {
+                return row; // La première case vide dans la colonne
+            }
+        }
+        return -1; // La colonne est pleine
+    }
+
+    private Circle getJeton(int columnIndex, int rowIndex) {
+        for (Node node : gridPane.getChildren()) {
+            if (GridPane.getColumnIndex(node) == columnIndex && GridPane.getRowIndex(node) == rowIndex) {
+                return (Circle) node;
+            }
+        }
+        return null; // Aucun jeton trouvé dans cette case
+    }
+
 }
