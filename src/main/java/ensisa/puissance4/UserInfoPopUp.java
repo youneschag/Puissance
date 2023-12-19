e27689ab524a6f7b0f8ed50c377abd212b11d6b1
Chaguer <chagueryounes@gmail.com>
2023-12-19 13:42:58 +0100

    pop up  of choices working, working on the similar ones during the app

--- src/main/java/ensisa/puissance4/UserInfoPopUp.java
+++ src/main/java/ensisa/puissance4/UserInfoPopUp.java
@@ -1,13 +1,18 @@
 package ensisa.puissance4;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.util.Callback;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class UserInfoPopUp {
+    private static Label labelSecondUsername = new Label("Nom du deuxième joueur:");
+    private static StringProperty selectedGameMode = new SimpleStringProperty("");
 
     public static UserInfo obtenirInfosUtilisateur() {
         // Créer une GridPane pour organiser les champs
@@ -17,6 +22,7 @@ public class UserInfoPopUp {
 
         // Ajouter le champ pour le nom d'utilisateur
         TextField usernameField = new TextField();
+        usernameField.setPromptText("Nom d'utilisateur");
         gridPane.add(new Label("Nom d'utilisateur:"), 0, 0);
         gridPane.add(usernameField, 1, 0);
 
@@ -28,13 +34,41 @@ public class UserInfoPopUp {
         gridPane.add(new Label("Choix du jeton:"), 0, 1);
         gridPane.add(tokenChoice, 1, 1);
 
-        // Ajouter le champ pour le choix du mode de jeu
-        List<String> gameModes = List.of("Human vs Human", "Human vs Computer");
-        ChoiceBox<String> gameModeChoice = new ChoiceBox<>();
-        gameModeChoice.getItems().addAll(gameModes);
-        gameModeChoice.setValue(gameModes.get(0));
+        // Créer une ToggleGroup pour les boutons radio
+        ToggleGroup gameModeToggleGroup = new ToggleGroup();
+
+        // Créer les boutons radio
+        RadioButton humanVsHumanRadioButton = new RadioButton("Human vs Human");
+        humanVsHumanRadioButton.setToggleGroup(gameModeToggleGroup);
+
+        RadioButton humanVsComputerRadioButton = new RadioButton("Human vs Computer");
+        humanVsComputerRadioButton.setToggleGroup(gameModeToggleGroup);
+
+        // Ajouter les boutons radio à la GridPane
         gridPane.add(new Label("Mode de jeu:"), 0, 2);
-        gridPane.add(gameModeChoice, 1, 2);
+        gridPane.add(humanVsHumanRadioButton, 1, 2);
+        gridPane.add(humanVsComputerRadioButton, 2, 2);
+
+        // Champ pour le deuxième utilisateur
+        TextField secondUsernameField = new TextField();
+        secondUsernameField.setVisible(false);
+        gridPane.add(labelSecondUsername, 0, 5);
+        gridPane.add(secondUsernameField, 1, 5);
+
+        // Ajouter un ChangeListener pour détecter le choix de l'utilisateur
+        gameModeToggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                // Mettre à jour le contenu en fonction du bouton radio sélectionné
+                selectedGameMode.set(((RadioButton) newValue).getText());
+
+                // Mettre à jour la visibilité du champ du deuxième utilisateur en fonction du choix du mode de jeu
+                secondUsernameField.setVisible("Human vs Human".equals(selectedGameMode.get()));
+                // Mettre à jour la visibilité du label "Nom d'utilisateur du deuxième joueur:"
+                labelSecondUsername.setVisible("Human vs Human".equals(selectedGameMode.get()));
+            }
+        });
+        // Définir le bouton radio initialement sélectionné (par exemple, "Human vs Computer")
+        humanVsComputerRadioButton.setSelected(true);
 
         // Ajouter le champ pour l'ordre de jeu
         List<String> orderChoices = List.of("Premier", "Deuxième");
@@ -45,21 +79,10 @@ public class UserInfoPopUp {
         gridPane.add(orderChoice, 1, 3);
 
         // Ajouter le champ pour la limite de temps
-        TextField timeLimitField = new TextField("0");
-        int timeLimit = Integer.parseInt(timeLimitField.getText());
-        gridPane.add(new Label("Limite de temps en secondes (0 : pas de limite):"), 0, 4);
+        TextField timeLimitField = new TextField("");
+        timeLimitField.setPromptText("0 : pas de limite");
+        gridPane.add(new Label("Limite de temps(en s):"), 0, 4);
         gridPane.add(timeLimitField, 1, 4);
-
-        // Ajouter le champ pour le deuxième utilisateur (visible seulement si "Human vs Human" est sélectionné)
-        TextField secondUsernameField = new TextField();
-        secondUsernameField.setVisible(false);
-        gridPane.add(new Label("Nom d'utilisateur du deuxième joueur:"), 0, 5);
-        gridPane.add(secondUsernameField, 1, 5);
-
-        // Écouter les changements dans le choix du mode de jeu
-        gameModeChoice.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            secondUsernameField.setVisible("Human vs Human".equals(newValue));
-        });
 
         // Boîte de dialogue principale
         Dialog<UserInfo> dialog = new Dialog<>();
@@ -72,21 +95,18 @@ public class UserInfoPopUp {
         dialog.getDialogPane().getButtonTypes().addAll(okButtonType, ButtonType.CANCEL);
 
         // Configurer la conversion des résultats
-        dialog.setResultConverter(new Callback<ButtonType, UserInfo>() {
-            @Override
-            public UserInfo call(ButtonType buttonType) {
-                if (buttonType == okButtonType) {
-                    return new UserInfo(
-                            usernameField.getText(),
-                            tokenChoice.getValue(),
-                            gameModeChoice.getValue(),
-                            secondUsernameField.getText(),
-                            orderChoice.getValue(),
-                            timeLimit
-                    );
-                }
-                return null;
+        dialog.setResultConverter(buttonType -> {
+            if (buttonType == okButtonType) {
+                return new UserInfo(
+                        usernameField.getText(),
+                        tokenChoice.getValue(),
+                        selectedGameMode.get(),
+                        secondUsernameField.getText(),
+                        orderChoice.getValue(),
+                        Integer.parseInt(timeLimitField.getText())
+                );
             }
+            return null;
         });
 
         // Afficher la boîte de dialogue et récupérer les résultats
